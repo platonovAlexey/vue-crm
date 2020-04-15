7b985889d54e98a068267b960c40256c8c98d391
platonovAlexey <kalyawe4ka@gmail.com>
2020-04-15 20:56:01 +0300

    add detail page to history

--- src/store/record.js
+++ src/store/record.js
@@ -25,5 +25,16 @@ export default {
         throw e;
       }
     },
+    async fetchRecordById({ dispatch, commit }, id) {
+      try {
+        const uid = await dispatch('getUid');
+        const records = (await firebase.database().ref(`/users/${uid}/records`).child(id).once('value')).val() || {};
+
+        return { ...records, id };
+      } catch (e) {
+        commit('setError', e);
+        throw e;
+      }
+    },
   },
 };
