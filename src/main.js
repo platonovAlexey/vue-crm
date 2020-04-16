8fec06cfca6b0a664e70ccfe8ee5a4f932051c70
platonovAlexey <kalyawe4ka@gmail.com>
2020-04-16 21:05:51 +0300

    add localize, profile page

--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@
 import Vue from 'vue';
 import dateFilter from '@/filters/date.filter';
 import currencyFilter from '@/filters/currency.filter';
+import localizeFilter from '@/filters/localize.filter';
 import tooltipDirective from '@/directives/tooltip.directive';
 import Vuelidate from 'vuelidate';
 import messagePlugin from '@/utils/message.plugin';
+import Paginate from 'vuejs-paginate';
 import App from './App.vue';
 import router from './router';
 import store from './store';
@@ -23,7 +25,9 @@ Vue.config.productionTip = false;
 
 Vue.filter('date', dateFilter);
 Vue.filter('currency', currencyFilter);
+Vue.filter('localize', localizeFilter);
 Vue.directive('tooltip', tooltipDirective);
+Vue.component('Paginate', Paginate);
 
 Vue.component('Loader', Loader);
 
--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,18 +1,33 @@
 <template>
   <div>
     <div class="page-title">
-      <h3>Профиль</h3>
+      <h3>{{'ProfileTitle' | localize}}</h3>
     </div>
 
-    <form class="form">
+    <form class="form" @submit.prevent="submitHandler">
       <div class="input-field">
         <input
             id="description"
             type="text"
+            v-model="name"
+            :class="{invalid: $v.name.$dirty && !$v.name.required}"
         >
         <label for="description">Имя</label>
-        <span
-              class="helper-text invalid">name</span>
+        <small
+          class="helper-text invalid"
+          v-if="$v.name.$dirty && !$v.name.required"
+          >
+          Введите имя
+        </small>
+      </div>
+
+      <div class="switch">
+        <label>
+          English
+          <input type="checkbox" v-model="isRuLocale">
+          <span class="lever"></span>
+          Русский
+        </label>
       </div>
 
       <button class="btn waves-effect waves-light" type="submit">
@@ -22,3 +37,53 @@
     </form>
   </div>
 </template>
+
+<script>
+import { required } from 'vuelidate/lib/validators';
+import { mapGetters, mapActions } from 'vuex';
+
+export default {
+  data: () => ({
+    name: '',
+    isRuLocale: true,
+  }),
+  validations: {
+    name: { required },
+  },
+  computed: {
+    ...mapGetters(['info']),
+  },
+  mounted() {
+    this.name = this.info.name;
+    this.isRuLocale = this.info.locale === 'ru-Ru';
+    setTimeout(() => {
+      // eslint-disable-next-line no-undef
+      M.updateTextFields();
+    }, 0);
+  },
+  methods: {
+    ...mapActions(['updateInfo']),
+    async submitHandler() {
+      if (this.$v.$invalid) {
+        this.$v.$touch();
+        return;
+      }
+
+      try {
+        await this.updateInfo({
+          name: this.name,
+          locale: this.isRuLocale ? 'ru-RU' : 'en-US',
+        });
+      } catch (e) {
+        console.log(e);
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+.switch{
+  margin-bottom: 2rem;
+}
+</style>
